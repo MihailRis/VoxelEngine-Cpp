82ee2e9d2f84db1f26122efa07bb8a7c4d667a2f
MihailRis <mexaruc@gmail.com>
2023-11-09 01:21:19 +0300

    Added missing files

---
+++ src/window/input.h
@@ -0,0 +1,47 @@
+#ifndef WINDOW_INPUT_H_
+#define WINDOW_INPUT_H_
+
+namespace keycode {
+    extern int ENTER;
+    extern int TAB;
+    extern int F1;
+    extern int F2;
+    extern int F3;
+    extern int F4;
+    extern int F5;
+    extern int F6;
+    extern int F7;
+    extern int F8;
+    extern int F9;
+    extern int F10;
+    extern int F11;
+    extern int F12;
+    extern int A;
+    extern int B;
+    extern int C;
+    extern int D;
+    extern int E;
+    extern int F;
+    extern int G;
+    extern int H;
+    extern int I;
+    extern int J;
+    extern int K;
+    extern int L;
+    extern int M;
+    extern int N;
+    extern int O;
+    extern int P;
+    extern int Q;
+    extern int R;
+    extern int S;
+    extern int T;
+    extern int U;
+    extern int V;
+    extern int W;
+    extern int X;
+    extern int Y;
+    extern int Z;
+}
+
+#endif // WINDOW_INPUT_H_
