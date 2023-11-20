3eb6e82631cc1fd2e962f8c20e9774e9332c7ab3
MihailRis <mexaruc@gmail.com>
2023-11-20 06:35:23 +0300

    Added missing key names

--- src/window/input.cpp
+++ src/window/input.cpp
@@ -80,6 +80,36 @@ const char* keycode::name(int code) {
             case GLFW_KEY_CAPS_LOCK: return "Caps-Lock";
             case GLFW_KEY_SPACE: return "Space";
             case GLFW_KEY_ESCAPE: return "Esc";
+            case GLFW_KEY_ENTER: return "Enter";
+            case GLFW_KEY_UP: return "Up";
+            case GLFW_KEY_DOWN: return "Down";
+            case GLFW_KEY_LEFT: return "Left";
+            case GLFW_KEY_RIGHT: return "Right";
+            case GLFW_KEY_BACKSPACE: return "Backspace";
+            case GLFW_KEY_F1: return "F1";
+            case GLFW_KEY_F2: return "F2";
+            case GLFW_KEY_F3: return "F3";
+            case GLFW_KEY_F4: return "F4";
+            case GLFW_KEY_F5: return "F5";
+            case GLFW_KEY_F6: return "F6";
+            case GLFW_KEY_F7: return "F7";
+            case GLFW_KEY_F8: return "F8";
+            case GLFW_KEY_F9: return "F9";
+            case GLFW_KEY_F10: return "F10";
+            case GLFW_KEY_F11: return "F11";
+            case GLFW_KEY_F12: return "F12";
+            case GLFW_KEY_DELETE: return "Delete";
+            case GLFW_KEY_HOME: return "Home";
+            case GLFW_KEY_END: return "End";
+            case GLFW_KEY_LEFT_SUPER: return "Left Super";
+            case GLFW_KEY_RIGHT_SUPER: return "Right Super";
+            case GLFW_KEY_PAGE_UP: return "Page Up";
+            case GLFW_KEY_PAGE_DOWN: return "Page Down";
+            case GLFW_KEY_INSERT: return "Insert";
+            case GLFW_KEY_PRINT_SCREEN: return "Print Screen";
+            case GLFW_KEY_NUM_LOCK: return "Num Lock";
+            case GLFW_KEY_MENU: return "Menu";
+            case GLFW_KEY_PAUSE: return "Pause";
             default:
                 return "Unknown";
         }
