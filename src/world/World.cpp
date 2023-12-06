f9a57582135b7347ef80aaa3fe1684f4095e23ba
MihailRis <mexaruc@gmail.com>
2023-12-06 18:01:14 +0300

    Lights fix

--- src/world/World.cpp
+++ src/world/World.cpp
@@ -43,9 +43,10 @@ void World::write(Level* level) {
 
 	for (size_t i = 0; i < chunks->volume; i++) {
 		shared_ptr<Chunk> chunk = chunks->chunks[i];
-		if (chunk == nullptr)
+		if (chunk == nullptr || !chunk->isLighted())
 			continue;
-		bool lightsUnsaved = !chunk->isLoadedLights() && settings.debug.doWriteLights;
+		bool lightsUnsaved = !chunk->isLoadedLights() && 
+							  settings.debug.doWriteLights;
 		if (!chunk->isUnsaved() && !lightsUnsaved)
 			continue;
 		wfile->put(chunk.get());
