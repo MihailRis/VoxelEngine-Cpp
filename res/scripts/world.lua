a29ee255501707fc72a3dc6c7c5a50b52d212a33
DomaJa <149813486+DSDemen@users.noreply.github.com>
2024-03-20 16:52:39 +0200

    World.lua

--- res/scripts/world.lua
+++ res/scripts/world.lua
@@ -1,3 +1,10 @@
--- use for engine development tests
--- must be empty in release
--- must not be modified by content-packs
+local stream_id
+
+function on_world_open()
+	stream_id = audio.play_stream_2d("sounds/musics/Subwoofer.ogg", 1, 1, "music", true)
+	print("All ok")
+end
+
+function on_world_quit()
+    audio.stop(stream_id)
+end
