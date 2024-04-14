549cffae87c8f6cfe39862f8fe3822fe11b2e8c1
MihailRis <mexaruc@gmail.com>
2024-04-14 15:12:23 +0300

    textbox tag fix

--- src/frontend/gui/gui_xml.cpp
+++ src/frontend/gui/gui_xml.cpp
@@ -263,7 +263,7 @@ static std::shared_ptr<UINode> readTextBox(UiXmlReader& reader, xml::xmlelement 
     if (element->has("supplier")) {
         auto supplier = scripting::create_wstring_supplier(
             reader.getEnvironment().getId(),
-            element->attr("consumer").getText(),
+            element->attr("supplier").getText(),
             reader.getFilename()
         );
         textbox->setTextSupplier(supplier);
