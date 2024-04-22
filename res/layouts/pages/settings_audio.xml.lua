201209887f1b5985af1906b14b9c106f146f2d96
MihailRis <mexaruc@gmail.com>
2024-04-22 19:04:00 +0300

    audio settings fix

--- res/layouts/pages/settings_audio.xml.lua
+++ res/layouts/pages/settings_audio.xml.lua
@@ -11,9 +11,9 @@ function new_volume_control(setting, id, name)
     document.tracks_panel:add("<label id='l_"..id.."'>-</label>")
     -- value track-bar
     document.tracks_panel:add(string.format(
-        "<trackbar id='t_%s' min='0' max='1' value='1' step='0.01' track-width='5' "..
+        "<trackbar id='t_%s' min='0' max='1' value='%s' step='0.01' track-width='5' "..
         " consumer='function(x) on_volume_change(%q, %q, %q, x) end'/>"
-    , id, setting, id, name))
+    , id, core.get_setting(setting), setting, id, name))
     refresh_label(setting, id, name)
 end
 
