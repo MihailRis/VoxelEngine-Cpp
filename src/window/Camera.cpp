a1776e015e170e5ab5575343f0b01836b4554107
MihailRis <mexaruc@gmail.com>
2024-05-24 05:22:14 +0300

    fixes

--- src/window/Camera.cpp
+++ src/window/Camera.cpp
@@ -21,10 +21,9 @@ void Camera::updateVectors(){
 }
 
 void Camera::rotate(float x, float y, float z){
-    rotation = glm::rotate(rotation, z, glm::vec3(0,0,1));
     rotation = glm::rotate(rotation, y, glm::vec3(0,1,0));
     rotation = glm::rotate(rotation, x, glm::vec3(1,0,0));
-
+    rotation = glm::rotate(rotation, z, glm::vec3(0,0,1));
     updateVectors();
 }
 
