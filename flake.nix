5f76d273c897035d65a3e8a9ee264d6eee73be2c
InfiniteCoder <info@infinitecoder.org>
2024-06-13 22:53:47 +0300

    Nix flakes

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+        devShells.default = with nixpkgs.legacyPackages.${system}; mkShell {
+          nativeBuildInputs = [ cmake pkg-config ];
+          buildInputs = [ glm glfw glew zlib libpng libvorbis openal luajit  ]; # libglvnd
+          packages = [ mesa freeglut entt ];
+          LD_LIBRARY_PATH = "${wayland}/lib:$LD_LIBRARY_PATH";
+        };
+    });
+}
