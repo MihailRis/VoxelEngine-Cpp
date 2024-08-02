2e7c0a109d2147bc808b9fe50493855e61089a6b
Vyacheslav Ivanov <islavaivanov76@gmail.com>
2024-08-02 05:41:47 +0300

    fix: optimization: PVS-Studio warning V810
    Cached the result of 'glm::vec3(size * 0.63f)' in a temporary variable to avoid multiple identical function calls and improve performance.
    
    Reported by: PVS-Studio
    Signed-off-by: Vyacheslav Ivanov <islavaivanov76@gmail.com>

--- src/graphics/render/BlocksPreview.cpp
+++ src/graphics/render/BlocksPreview.cpp
@@ -50,9 +50,10 @@ std::unique_ptr<ImageData> BlocksPreview::draw(
                 }
                 offset = glm::vec3(1, 1, 0.0f);
                 shader->uniformMatrix("u_apply", glm::translate(glm::mat4(1.0f), offset));
+                glm::vec3 scaledSize = glm::vec3(size * 0.63f);
                 batch->cube(
-                    -hitbox * glm::vec3(size * 0.63f)*0.5f * glm::vec3(1,1,-1),
-                    hitbox * glm::vec3(size * 0.63f), 
+                    -hitbox * scaledSize * 0.5f * glm::vec3(1,1,-1),
+                    hitbox * scaledSize,
                     texfaces, glm::vec4(1.0f), 
                     !def->rt.emissive
                 );
