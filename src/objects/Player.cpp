e014931e1e8aeb1e1793288b5f65524e9c354549
MihailRis <mexaruc@gmail.com>
2024-08-03 22:36:18 +0300

    fix: selection update latency

--- src/objects/Player.cpp
+++ src/objects/Player.cpp
@@ -140,7 +140,6 @@ void Player::updateSelectedEntity() {
     selectedEid = selection.entity;
 }
 
-#include "../window/Window.hpp"
 void Player::postUpdate() {
     auto entity = level->entities->get(eid);
     if (!entity.has_value()) {
