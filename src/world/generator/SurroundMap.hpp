cf3f263f7254eadf9eceaf15a1416807968e200b
MihailRis <mexaruc@gmail.com>
2024-08-28 11:41:04 +0300

    update SurroundMap (WIP)

--- src/world/generator/SurroundMap.hpp
+++ src/world/generator/SurroundMap.hpp
@@ -10,11 +10,13 @@ class SurroundMap {
         /// @brief Level is increased when all surrounding (8) entries having
         // greather or equal confirmed level
         int level = 0;
-        /// @brief number of surrounding entries having greather or equal
-        /// confirmed level
-        int surrounding = 0;
+        /// @brief bits storing surrounding entries having greather or equal
+        /// confirmed level.
+        /// 0 - is x=-1,y=-1 offset, 1 is x=0,y=-1, ... 7 is x=1,y=1
+        /// (Prevents extra access to the entries hashmap)
+        uint8_t surrounding = 0x0;
         /// @brief level confirmed status (entry is ready to expand)
-        bool confirmed = true;
+        bool confirmed = false;
         /// @brief mark used on sweep event (extra isles garbage collection)
         bool marked = false;
     };
@@ -24,15 +26,22 @@ public:
     void resetMarks();
 
     /// @brief Mark all connected entries
-    /// @param point origin point
-    void markIsle(const glm::ivec2& point);
+    /// @param origin origin point
+    void markIsle(const glm::ivec2& origin);
 
     /// @brief Erase all non-marked isles
     /// @return erased entries positions
     std::vector<glm::ivec2> sweep();
 
-    /// @brief Expand all confirmed entries with specified level
+    /// @brief Attempt to upgrade all confirmed entries with specified level
     /// @param level target entries level
     /// @return All upgraded entries positions 
-    std::vector<glm::ivec2> expand(int level);
+    std::vector<glm::ivec2> upgrade();
+    
+    /// @brief Create entry if does not exist
+    /// @param origin entry position
+    /// @return true if new entry has been created
+    bool createEntry(const glm::ivec2& origin);
+
+    void getLevels(unsigned char* out, int width, int height, int ox, int oy) const;
 };
--- test/world/generator/SurroundMap.cpp
+++ test/world/generator/SurroundMap.cpp
@@ -3,5 +3,11 @@
 #include "world/generator/SurroundMap.hpp"
 
 TEST(SurroundMap, InitTest) {
+    int w = 8;
+    int h = 8;
+
     SurroundMap map;
+    map.createEntry({w/2, h/2});
+    map.createEntry({w/2+1, h/2+1});
+    EXPECT_EQ(map.upgrade().size(), 12);
 }
