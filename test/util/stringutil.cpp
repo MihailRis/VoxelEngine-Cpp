bd87a586d80c83ead9c068075f08ffb44734e0a6
MihailRis <mexaruc@gmail.com>
2024-08-29 13:47:18 +0300

    fix stringutil test

--- test/util/stringutil.cpp
+++ test/util/stringutil.cpp
@@ -4,7 +4,7 @@
 
 TEST(stringutil, crop_utf8) {
     // Project source files must be UTF-8 encoded
-    std::string str = "пример";
+    std::string str = u8"пример";
     str = str.substr(0, util::crop_utf8(str, 7));
-    EXPECT_EQ(str, "при");
+    EXPECT_EQ(str, u8"при");
 }
