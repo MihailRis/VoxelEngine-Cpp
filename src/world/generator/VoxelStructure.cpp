9dd4e9640dd47e787a5c0eaa137e6efb4ee4e9b7
MihailRis <mexaruc@gmail.com>
2024-09-22 02:12:35 +0300

    fix structures rotation

--- src/world/generator/VoxelStructure.cpp
+++ src/world/generator/VoxelStructure.cpp
@@ -109,7 +109,7 @@ std::unique_ptr<VoxelStructure> VoxelStructure::rotated(const Content& content) 
                 if (def.rotations.name == BlockRotProfile::PANE_NAME ||
                       def.rotations.name == BlockRotProfile::PIPE_NAME){
                     if (voxel.state.rotation < 4) {
-                        voxel.state.rotation = (voxel.state.rotation + 3) & 0b11;
+                        voxel.state.rotation = (voxel.state.rotation + 1) & 0b11;
                     }
                 }
             }
