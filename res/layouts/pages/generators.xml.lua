c0c0104b5ef3e3e7f919a07537438d9fc2f9caa6
MihailRis <mexaruc@gmail.com>
2024-10-15 04:12:30 +0300

    sort generators in ui

--- res/layouts/pages/generators.xml.lua
+++ res/layouts/pages/generators.xml.lua
@@ -2,13 +2,18 @@ settings = session.get_entry('new_world')
 
 function on_open()
     local names = generation.get_generators()
-    table.sort(names)
+    local keys = {}
+    for key in pairs(names) do
+        table.insert(keys, key)
+    end
+    table.sort(keys)
 
     local panel = document.root
-    for k, caption in pairs(names) do
+    for _, key in ipairs(keys) do
+        local caption = names[key]
         panel:add(gui.template("generator", {
-            callback=string.format("settings.generator=%q menu:back()", k),
-            id=k,
+            callback=string.format("settings.generator=%q menu:back()", key),
+            id=key,
             name=settings.generator_name(caption)
         }))
     end
