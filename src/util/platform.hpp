6964a186050409a6c8d9cb0220a915eb3664ac3a
MihailRis <mexaruc@gmail.com>
2024-10-25 13:58:17 +0300

    add platform::open_folder

--- src/util/platform.hpp
+++ src/util/platform.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <string>
+#include <filesystem>
 
 namespace platform {
     void configure_encoding();
-    // @return environment locale in ISO format ll_CC
+    /// @return environment locale in ISO format ll_CC
     std::string detect_locale();
+    /// @brief Open folder using system file manager application
+    /// @param folder target folder
+    void open_folder(const std::filesystem::path& folder);
 }
