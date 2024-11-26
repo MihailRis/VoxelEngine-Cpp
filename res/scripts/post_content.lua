1cd890ff8839cca7c0e1110da8d1a569bf57cb89
MihailRis <mexaruc@gmail.com>
2024-11-26 11:21:04 +0300

    add config/user-props.toml combined object

--- res/scripts/post_content.lua
+++ res/scripts/post_content.lua
@@ -1,4 +1,19 @@
-function make_read_only(t)
+local user_props = file.read_combined_object("config/user-props.toml")
+local names = {}
+for name, _ in pairs(user_props) do
+    table.insert(names, name)
+end
+-- remove undefined properties
+for id, blockprops in pairs(block.properties) do
+    for propname, value in pairs(blockprops) do
+        if propname:find(':') and not table.has(names, propname) then
+            print("erase property", propname)
+            blockprops[propname] = nil
+        end
+    end
+end
+
+local function make_read_only(t)
     setmetatable(t, {
         __newindex = function()
             error("table is read-only")
