d6ccdf98717a9152311f580f128d5e3dbb8710de
MihailRis <mexaruc@gmail.com>
2024-12-22 02:27:00 +0300

    add new Bytearray:insert overloads

--- src/logic/scripting/lua/usertypes/lua_type_bytearray.cpp
+++ src/logic/scripting/lua/usertypes/lua_type_bytearray.cpp
@@ -43,8 +43,19 @@ static int l_insert(lua::State* L) {
     if (static_cast<size_t>(index) > data.size()) {
         return 0;
     }
-    auto value = tointeger(L, 3);
-    data.insert(data.begin() + index, static_cast<ubyte>(value));
+    if (lua::isnumber(L, 3)) {
+        auto value = tointeger(L, 3);
+        data.insert(data.begin() + index, static_cast<ubyte>(value));
+    } else if (lua::istable(L, 3)) {
+        std::vector<ubyte> temp;
+        lua::read_bytes_from_table(L, 3, temp);
+        data.insert(data.begin() + index, temp.begin(), temp.end());
+    } else if (auto extension = lua::touserdata<LuaBytearray>(L, 3)) {
+        const std::vector<ubyte>& src = extension->data();
+        data.insert(data.begin() + index, src.begin(), src.end());
+    } else {
+        throw std::runtime_error("integer/table/Bytearray expected");
+    }
     return 0;
 }
 
