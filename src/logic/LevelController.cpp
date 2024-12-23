6a8afe2795cbd1332367b101f47db6efac1d3823
MihailRis <mexaruc@gmail.com>
2024-12-23 14:01:35 +0300

    fix

--- src/logic/LevelController.cpp
+++ src/logic/LevelController.cpp
@@ -47,9 +47,7 @@ LevelController::LevelController(
             );
             chunks->update(16, 1, 0, *player);
             if (player->chunks->get(
-                    std::floor(position.x),
-                    std::floor(position.y),
-                    std::floor(position.z)
+                    std::floor(position.x), 0, std::floor(position.z)
                 )) {
                 confirmed++;
             }
