bf6804dbdec7d5f57aaf1b9a9f67db604734d2f2
Stepanov Igor <ygrik2003@yandex.ru>
2024-12-27 21:27:38 +0300

    Add Tracy Profiler for windows

---
+++ src/engine/Profiler.cpp
@@ -0,0 +1,15 @@
+#include "Profiler.hpp"
+
+#if defined(VOXELENGINE_PROFILER)
+  #if defined(VOXELENGINE_TRACY_PROFILER)
+void* operator new(std ::size_t count) {
+    auto ptr = malloc(count);
+    TracyAlloc(ptr, count);
+    return ptr;
+}
+void operator delete(void* ptr) noexcept {
+    TracyFree(ptr);
+    free(ptr);
+}
+  #endif
+#endif
