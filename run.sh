fcb5c7fc1dc1dc17ae866eaa986d97d9614b6a47
Alexei Kartashov <alexei.zebra@yandex.ru>
2025-01-30 01:52:20 +0300

    run script: add help page

--- run.sh
+++ run.sh
@@ -31,13 +31,28 @@ function norun {
 }
 
 
+function help {
+    echo "[RUN SCRIPT] Usage: ./run [ARGUMENT]..."
+    echo "[RUN SCRIPT] Arguments:"
+    echo "[RUN SCRIPT]     -d, --delete     Delete build directory"
+    echo "[RUN SCRIPT]     -b, --build      Build project"
+    echo "[RUN SCRIPT]     -r, --rebuild    Rebuild project"
+    echo "[RUN SCRIPT]     -R, --norun      Build without run"
+    echo "[RUN SCRIPT]     -h, --help       Print this page"
+}
+
+
 while [ -n "$1" ]; do
     case "$1" in
         -d | --delete) delete ;;
         -b | --build) build ;;
         -r | --rebuild) rebuild ;;
         -R | --norun) norun ;;
+        -h | --help) help
+                     norun
+                     break ;;
         *) echo "[RUN SCRIPT] Unknown argument: $1"
+           help
            norun
            break ;;
     esac
