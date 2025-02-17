53848438924e17edaad81e1438196bab74b64e2b
MihailRis <mexaruc@gmail.com>
2025-02-17 16:58:03 +0300

    fix bazalt_breaker with infinite items enabled

--- res/content/base/scripts/bazalt_breaker.lua
+++ res/content/base/scripts/bazalt_breaker.lua
@@ -1,4 +1,6 @@
 function on_block_break_by(x, y, z, pid)
     block.set(x, y, z, 0, 0)
-    inventory.use(player.get_inventory(pid))
+    if not player.is_infinite_items(pid) then
+        inventory.use(player.get_inventory(pid))
+    end
 end
