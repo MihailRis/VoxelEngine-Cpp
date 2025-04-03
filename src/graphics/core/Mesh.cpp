1dc9557451ec8eced7d713b2ed4de8484322a67a
REDxEYE <med45c@gmail.com>
2025-04-03 14:26:25 +0300

    Remove unnecessary glew includes. Remove copyright from Mesh.cpp Add default initializers for VertexAttribute

--- src/graphics/core/Mesh.cpp
+++ src/graphics/core/Mesh.cpp
@@ -1,7 +1,3 @@
-//
-// Created by RED on 24.03.2025.
-//
-
 #include "graphics/core/Mesh.hpp"
 
  int MeshStats::meshesCount = 0;
--- src/graphics/core/Mesh.inl
+++ src/graphics/core/Mesh.inl
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <GL/glew.h>
-
 #include "MeshData.hpp"
 
 template<typename VertexStructure>
