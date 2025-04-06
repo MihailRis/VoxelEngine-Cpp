c3bc084e766eba320f6662703649ab7ddd0bd16e
MihailRis <mexaruc@gmail.com>
2025-04-06 12:45:58 +0300

    update Shader class

--- src/graphics/core/Shader.hpp
+++ src/graphics/core/Shader.hpp
@@ -21,14 +21,15 @@ public:
     ~Shader();
 
     void use();
-    void uniformMatrix(const std::string&, glm::mat4 matrix);
+    void uniformMatrix(const std::string&, const glm::mat4& matrix);
     void uniform1i(const std::string& name, int x);
     void uniform1f(const std::string& name, float x);
     void uniform2f(const std::string& name, float x, float y);
-    void uniform2f(const std::string& name, glm::vec2 xy);
-    void uniform2i(const std::string& name, glm::ivec2 xy);
+    void uniform2f(const std::string& name, const glm::vec2& xy);
+    void uniform2i(const std::string& name, const glm::ivec2& xy);
     void uniform3f(const std::string& name, float x, float y, float z);
-    void uniform3f(const std::string& name, glm::vec3 xyz);
+    void uniform3f(const std::string& name, const glm::vec3& xyz);
+    void uniform4f(const std::string& name, const glm::vec4& xyzw);
 
     /// @brief Create shader program using vertex and fragment shaders source.
     /// @param vertexFile vertex shader file name
